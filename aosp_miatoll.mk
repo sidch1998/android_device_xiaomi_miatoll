195d2baac2d2c983268e58de328a0fc671e24c19
sidch1998 <sommojeet@gmail.com>
2020-11-22 04:43:10 +0000

    ssos bringup

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -8,13 +8,17 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+#GAPPS
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -22,5 +26,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+#Shapeshift stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_OOS_RECORDER := true
