50b981cde3a3b186a205cb08384308674d1bd05c
sidch1998 <sommojeet@gmail.com>
2020-11-29 12:07:53 +0000

    colt bringup

--- colt_miatoll.mk
+++ colt_miatoll.mk
@@ -8,24 +8,16 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-#GAPPS
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1080x2400
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := colt_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Shapeshift stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_OOS_RECORDER := true
