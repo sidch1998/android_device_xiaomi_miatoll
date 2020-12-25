15e75045e641a443e6980d6614325bcc38ee3d78
sidch1998 <sommojeet@gmail.com>
2020-12-25 10:28:03 +0000

    zenx

--- zenx_miatoll.mk
+++ zenx_miatoll.mk
@@ -8,9 +8,10 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/zenx/config/common_full_phone.mk)
 
 #GAPPS
+IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
@@ -18,14 +19,10 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := zenx_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Shapeshift stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_OOS_RECORDER := true
