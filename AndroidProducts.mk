dca0bedcae458afe3beb540b28a0457625061444
sidch1998 <sommojeet@gmail.com>
2021-01-14 12:52:08 +0000

    nusantara bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_miatoll.mk
+    $(LOCAL_DIR)/nad_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_miatoll-user \
-    aosp_miatoll-userdebug \
-    aosp_miatoll-eng
+    nad_miatoll-user \
+    nad_miatoll-userdebug \
+    nad_miatoll-eng
--- nad_miatoll.mk
+++ nad_miatoll.mk
@@ -8,24 +8,17 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-#GAPPS
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+#$(call inherit-product, packages/apps/NusantaraParts/nadproject.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := nad_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Shapeshift stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_OOS_RECORDER := true
