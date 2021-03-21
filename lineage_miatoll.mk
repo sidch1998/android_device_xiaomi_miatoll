1562427f6a0936906ced8546f8ae47d65646419a
sidch1998 <sommojeet@gmail.com>
2021-03-21 12:33:36 +0000

    init sakura

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -8,17 +8,19 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 #GAPPS
 TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+SAKURA_GAPPS := true
+
+LAWNCHAIR_OPTOUT := false
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -26,6 +28,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#Shapeshift stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_OOS_RECORDER := true
